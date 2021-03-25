0836f15e7e24c9faf6d033ccae34b82a789df568
Ali Bas <alibas01@gmail.com>
2021-03-25 14:14:13 -0400

    resume page in progress

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import "../styles/css/welcome.css"
+import "../styles/home.scss"
 
 import Button from "./Button";  
+import Footer from "./Footer"; 
 
 export default function Resume(props) {  
   return(
-    <div>
-      I am resume
+    <div className="allpage">
+      <section className="yazi">
+        <h3>Hello, I am Ali and
+            I like coding,
+            which is what I do
+            in Toronto, ON.
+            You can contact me at alibas01@gmail.com
+        </h3>
+        <div className="tiger">
+            <img className="gold-tiger" src='https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/Gold-tiger.png' />
+        </div>
+      </section>
+      <div className="resume-page">
+        <section className="menu">
+                <Button 
+                  text="Home" 
+                  disabled={false} 
+                  className="--welcome"
+                />
+                <Button 
+                  text="Projects" 
+                  disabled={false} 
+                  className="--welcome"
+                />
+                    
+        </section>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   )
 }
