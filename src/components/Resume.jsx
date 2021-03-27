7bc3e9149313ccb72f66c0caea27cc88849cf303
Ali Bas <alibas01@gmail.com>
2021-03-27 19:18:05 -0400

    first version completed

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -11,28 +11,26 @@ export default function Resume(props) {
     <div className="allpage">
       <Nav title="Resumé" menu="Projects"/>
       <section className="yazi">
-        <div>
         <h3>Hello, I am Ali and
             I like coding,
             which is what I do
             in Toronto, ON.
-            You can contact me at 
+            You can contact me by the links at the bottom of the page! Click on the resume image for pdf.
         </h3>
-        <a href={'mailto:alibas01@gmail.com'}>alibas01@gmail.com</a>
-        </div>
+        {/* <a href={'mailto:alibas01@gmail.com'}>alibas01@gmail.com</a> */}
         <img className="gold-tiger" src='https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/Gold-tiger.png' />
       </section>
       <div className="resume-page">
-      <Link to='/'>
+      <a href='https://drive.google.com/file/d/1Kvo5H4sfn10z1jT_iAqNp3ZVk6ff0znD/view?usp=sharing'>
         <img
-          className='navbar-logo'
+          className='resume-logo'
           alt='home'
           src={
             'https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/Resume.png'
             //'https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/tigeringlasses.png'
           }
         ></img>
-      </Link>
+      </a>
        
       </div>
       <div>
