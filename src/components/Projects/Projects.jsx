3abe70769dca206a0cc32d1754e0228c4fd4ac3d
Ali Bas <alibas01@gmail.com>
2021-03-27 11:20:23 -0400

    projects page is completed, navbar to add..

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -20,6 +20,18 @@ export default function Projects(props) {
           <h4>* TinyApp</h4>
         </a>
             <p> Stack: NodeJS, Express | EJS, Bcrypt, cookie-sessions</p>
+        <a href={'https://github.com/alibas01/jungle'}>
+          <h4>* Jungle</h4>
+        </a>
+            <p> Stack: Ruby, Rails, Stripe, HTML, scss</p>
+        <a href={'https://github.com/alibas01/lotide2'}>
+          <h4>* Lotide</h4>
+        </a>
+            <p> Stack: JS/ NPM library written for learning purposes for example Lodash functions </p>
+        <a href={'https://github.com/alibas01/Classifying-Nature-Pictures/blob/master/Capstone%20Project%202%20Final%20Report.pdf'}>
+          <h4>* Classifying Nature Pictures</h4>
+        </a>
+            <p> Deep Learning Model with Tensorflow and Keras. </p>
       </section>
               <div className="tesla">
                   <img className="tesla-img" src='https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/tesla.jpg' />
@@ -33,6 +45,14 @@ export default function Projects(props) {
           <h4>* Aviation Safety Model</h4>
         </a>
           <p> NLP model on ASRS data set after going through data cleaning, data wrangling, exploratory data analysis, and some unique data visualization features. Stack: Python with Numpy, Pandas, Scipy, Seaborn, SKlearn</p>
+        <a href={'https://github.com/alibas01/tweeter'}>
+          <h4>* Tweeter</h4>
+        </a>
+          <p> Stack: JS Express, Node.js, HTML, CSS, jQuery, Ajax</p>
+        <a href={'https://github.com/alibas01/wiki-map-app'}>
+          <h4>* Wiki-Map-App</h4>
+        </a>
+          <p> Stack: JS Express, Node.js, HTML, SCSS, Postgres, EJS</p>
       </section>  
       </div>
 
