1aba8aa0185ce4f9412bfd8b85086c202bdb7e90
Ali Bas <alibas01@gmail.com>
2021-04-09 15:53:02 -0400

    plausible statistics

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/home.scss"
+import "../styles/home.scss";
 
-import Button from "./Button";  
-import Footer from "./Footer";  
+import Button from "./Button";
+import Footer from "./Footer";
 
-export default function Home(props) {  
-  return(
+export default function Home(props) {
+  return (
     <div className="allpage">
       <h1>Welcome to my personal page!</h1>
       <div className="buttig">
-              <div className="tiger">
-                  <img className="dark-tiger" alt="dtiger" src='https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/Dark-tiger2.png' />
-              </div>
+        <div className="tiger">
+          <img
+            className="dark-tiger"
+            alt="dtiger"
+            src="https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/Dark-tiger2.png"
+          />
+        </div>
         <section className="menu">
-          <Link to='/resume'>
-            <Button 
-              text="Resumé" 
-              disabled={false} 
-              className="--welcome"
-            />
+          <Link to="/resume">
+            <Button text="Resumé" disabled={false} className="--welcome" />
           </Link>
-          <Link to='/projects'>
-            <Button 
-              text="Projects" 
-              disabled={false} 
-              className="--welcome"
-            />
+          <Link to="/projects">
+            <Button text="Projects" disabled={false} className="--welcome" />
           </Link>
-          <Link to='/resume'>
-            <Button 
-              text="Blog (coming soon)" 
-              disabled={false} 
-              className="--welcome"
-            />
-          </Link>
-            <Button 
-              text="Tuuitter (coming soon)" 
-              disabled={false} 
-              className="--welcome"
-            />
-                {/* <Button 
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://plausible.io/alibas.ca"
+          >
+            <Button text="Statistics" disabled={false} className="--welcome" />
+          </a>
+          <Button
+            text="Tuuitter (coming soon)"
+            disabled={false}
+            className="--welcome"
+          />
+          {/* <Button 
                   text="Fly-Fi" 
                   disabled={false} 
                   className="--welcome"
@@ -51,5 +47,5 @@ export default function Home(props) {
         <Footer />
       </div>
     </div>
-  )
+  );
 }
