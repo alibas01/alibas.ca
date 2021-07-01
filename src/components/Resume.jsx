ccdce19c3b0dfd2959ef7566924cb4e373ce94b3
Ali Bas <alibas01@gmail.com>
2021-07-01 15:48:42 -0400

    new resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,41 +1,47 @@
 import React from "react";
 //import { Link } from "react-router-dom";
-import "../styles/home.scss"
+import "../styles/home.scss";
 
-//import Button from "./Button";  
-import Footer from "./Footer"; 
-import Nav from "./Nav"; 
+//import Button from "./Button";
+import Footer from "./Footer";
+import Nav from "./Nav";
 
-export default function Resume(props) {  
-  return(
+export default function Resume(props) {
+  return (
     <div className="allpage">
-      <Nav title="Resumé" menu="Projects"/>
+      <Nav title="Resumé" menu="Projects" />
       <section className="yazi">
-        <h3>Hello, I am Ali and
-            I like coding,
-            which is what I do
-            in Toronto, ON.
-            You can contact me by the links at the bottom of the page! Click on the resume image for pdf.
+        <h3>
+          Hello, I am Ali and I like coding, which is what I do in Toronto, ON.
+          You can contact me by the links at the bottom of the page! Click on
+          the resume image for pdf.
         </h3>
         {/* <a href={'mailto:alibas01@gmail.com'}>alibas01@gmail.com</a> */}
-        <img className="gold-tiger" alt="gtiger" src='https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/Gold-tiger.png' />
+        <img
+          className="gold-tiger"
+          alt="gtiger"
+          src="https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/Gold-tiger.png"
+        />
       </section>
       <div className="resume-page">
-      <a target="_blank" rel="noopener noreferrer" href='https://drive.google.com/file/d/1Kvo5H4sfn10z1jT_iAqNp3ZVk6ff0znD/view?usp=sharing'>
-        <img
-          className='resume-logo'
-          alt='home'
-          src={
-            'https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/Resume.png'
-            //'https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/tigeringlasses.png'
-          }
-        ></img>
-      </a>
-       
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://drive.google.com/file/d/1fEpkga6Y2nNk6BCawvU9-Oxh0z6NQzWK/view"
+        >
+          <img
+            className="resume-logo"
+            alt="home"
+            src={
+              "https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/Resume.png"
+              //'https://raw.githubusercontent.com/alibas01/alibas.ca/master/public/images/tigeringlasses.png'
+            }
+          ></img>
+        </a>
       </div>
       <div>
         <Footer />
       </div>
     </div>
-  )
+  );
 }
