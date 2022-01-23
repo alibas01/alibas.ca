88f68c15bf658fecdbf15288fe9a98c2a58bcece
Ali Bas <alibas01@gmail.com>
2022-01-23 15:54:15 -0500

    new pages template mywordle and minesweeper

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Home from './components/Home';
 import Resume from './components/Resume';
 import Projects from './components/Projects/Projects';
+import MineSweeper from './components/MineSweeper/MineSweeper';
+import MyWordle from './components/MyWordle/MyWordle';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -12,6 +14,8 @@ export default function App() {
           <Route exact path='/' render={(props) => <Home />} />
           <Route exact path='/resume' render={(props) => <Resume />} />
           <Route exact path='/projects' render={(props) => <Projects />} />
+          <Route exact path='/minesweeper' render={(props) => <MineSweeper />} />
+          <Route exact path='/mywordle' render={(props) => <MyWordle />} />
         </Switch>
       </Router>
     </div>
