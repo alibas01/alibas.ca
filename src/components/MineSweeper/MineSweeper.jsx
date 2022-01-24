0f121185d8dc39ce878021786810add03d8a3d87
Ali Bas <alibas01@gmail.com>
2022-01-24 17:30:55 -0500

    minesweeper imports

--- src/components/MineSweeper/MineSweeper.jsx
+++ src/components/MineSweeper/MineSweeper.jsx
@@ -1,6 +1,69 @@
 
-export default function MineSweeper(props) {
+import React from "react";
+import useConditionsData from "../../hooks/useConditionsData";
+import "../../App.css";
+import Nav from "./Nav";
+import Table from "./Table";
+
+function App() {
+  const {
+    difficulty,
+    setDifficulty,
+    table,
+    show,
+    setShow,
+    flagCount,
+    setFlagCount,
+    freeze,
+    setFreeze,
+    start,
+    setStart,
+    end,
+    setEnd,
+    trueFlagCount,
+    setTrueFlagCount
+  } = useConditionsData();
+
+  const adjustClass = function(difficulty) {
+    let clas = "layout-";
+    if (difficulty === "Easy") {
+      return clas + "Easy";
+    } else if (difficulty === "Medium") {
+      return clas + "Medium";
+    } else {
+      return clas + "Hard";
+    }
+  }
+
   return (
-<h1> HELLO, this is minesweeper </h1>
+    <div className={adjustClass(difficulty)}>
+      <Nav
+        difficulty={difficulty}
+        setDifficulty={setDifficulty}
+        show={show}
+        setShow={setShow}
+        flagCount={flagCount}
+        setFlagCount={setFlagCount}
+        freeze={freeze}
+        setFreeze={setFreeze}
+        start={start}
+        setStart={setStart}
+        setEnd={setEnd}
+        end={end}
+      />
+      <Table
+        table={table}
+        difficulty={difficulty}
+        flagCount={flagCount}
+        setFlagCount={setFlagCount}
+        freeze={freeze}
+        setFreeze={setFreeze}
+        end={end}
+        trueFlagCount={trueFlagCount}
+        setTrueFlagCount={setTrueFlagCount}
+      />
+    </div>
   );
 }
+
+export default App;
